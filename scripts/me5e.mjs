dfef59d2399b921d80d76234587c6eeb6320e763
Parker <starrpe@gmail.com>
2024-08-19 20:12:04 -0400

    everything working without throwing any errors

--- scripts/me5e.mjs
+++ scripts/me5e.mjs
@@ -1,7 +1,7 @@
 Hooks.once("init", () => {
   // Add ME5E 'spellcasting' schools
   CONFIG.DND5E.spellSchools.btc = {
-    label: "hBiotics",
+    label: "Biotics",
     icon: "assets\\icons\\ablbiotics.svg",
     fullKey: "biotics",
     reference: "",
@@ -67,127 +67,107 @@ Hooks.once("init", () => {
     label: "ME5E.ConsumTypeNarcotic",
   };
 
-  // Add ME5E weapon properties
-  foundry.utils.mergeObject(CONFIG.DND5E.itemProperties, {
-    arc: { label: "Arc" },
-    bst: { label: "Burst Fire" },
-    dtp: { label: "Double Tap" },
-    het: { label: "Heat" },
-    hip: { label: "Hip Fire" },
-    snt: { label: "Silent" },
-    coi: { label: "Recoil" },
-  });
+  // Add ME5E item properties and push to validProperties so they're available on item sheets
+  const me5eItemProps = {
+    arc: "Arc",
+    bst: "Burst Fire",
+    dtp: "Double Tap",
+    het: "Heat",
+    hip: "Hip Fire",
+    snt: "Silent",
+    coi: "Recoil",
+  };
+
+  for (const [k, v] of Object.entries(me5eItemProps)) {
+    CONFIG.DND5E.itemProperties[k] = { label: v };
+    CONFIG.DND5E.validProperties.weapon.add(k);
+  }
 
   // Add ME5E currency
 
   CONFIG.DND5E.currencies = {
-    pp: {
-      label: "ME5E.Currency",
-      abbreviation: "C",
-      conversion: 0.1,
+    credits: {
+      label: "Credits",
+      abbreviation: "creds",
+      conversion: 1,
+    },
+  };
+  // Add ME5E condition types
+  const me5eConditions = {
+    indoctrinated: {
+      label: "Indoctrinated",
+      icon: "assets\\icons\\statusindoctrinated.svg",
+    },
+    lifted: {
+      label: "Lifted",
+      icon: "assets\\icons\\statuslifted.svg",
+    },
+    primed: {
+      label: "Primed",
+      icon: "assets\\icons\\statusprimed.svg",
+    },
+    targeting: {
+      label: "Targeting",
+      icon: "assets\\icons\\statustargeting.svg",
     },
   };
 
-  // Add ME5E condition types
-
-  CONFIG.DND5E.conditionTypes.indoctrinated = {
-    label: "ME5E.CondTypeIndoc",
-    icon: "assets\\icons\\statusindoctrinated.svg",
-    pseudo: true,
-  };
-  CONFIG.DND5E.conditionTypes.lifted = {
-    label: "ME5E.CondTypeLift",
-    icon: "assets\\icons\\statuslifted.svg",
-    pseudo: true,
-  };
-  CONFIG.DND5E.conditionTypes.primed = {
-    label: "ME5E.CondTypePrime",
-    icon: "assets\\icons\\statusprimed.svg",
-    pseudo: true,
-  };
-  CONFIG.DND5E.conditionTypes.targeting = {
-    label: "ME5E.CondTypeTarget",
-    icon: "assets\\icons\\statustargeting.svg",
-    pseudo: true,
-  };
+  for (const [k, v] of Object.entries(me5eConditions)) {
+    CONFIG.statusEffects.push({
+      id: k,
+      _id: dnd5e.utils.staticID(`dnd5e${k}`),
+      name: v.label,
+      img: v.icon,
+      reference: "",
+    });
+    CONFIG.DND5E.conditionTypes[k] = { ...v, pseudo: false };
+    CONFIG.DND5E.statusEffects[k] = { name: v.label, icon: v.icon };
+  }
 
   // Add ME5E weapon IDs
-  CONFIG.DND5E.weaponIds.assaultr = "2i9xfag1ztMVwomC";
-  CONFIG.DND5E.weaponIds.heavyp = "QBOx8pTvTIvZknD7";
-  CONFIG.DND5E.weaponIds.smg = "off175x9LLykWjz6";
-  CONFIG.DND5E.weaponIds.shotgun = "pf5CuxCVkC1hHD5A";
-  CONFIG.DND5E.weaponIds.sniperr = "dliOzyRmjOHuLVCx";
-  CONFIG.DND5E.weaponIds.heavyw = "it7NrU6lg2OrQF0S";
-});
-
-Hooks.once("setup", () => {
+  CONFIG.DND5E.weaponIds.assaultr =
+    "Compendium.fvtt-me5e.items-me5e.Item.2i9xfag1ztMVwomC";
+  CONFIG.DND5E.weaponIds.heavyp =
+    "Compendium.fvtt-me5e.items-me5e.Item.QBOx8pTvTIvZknD7";
+  CONFIG.DND5E.weaponIds.smg =
+    "Compendium.fvtt-me5e.items-me5e.Item.off175x9LLykWjz6";
+  CONFIG.DND5E.weaponIds.shotgun =
+    "Compendium.fvtt-me5e.items-me5e.Item.pf5CuxCVkC1hHD5A";
+  CONFIG.DND5E.weaponIds.sniperr =
+    "Compendium.fvtt-me5e.items-me5e.Item.dliOzyRmjOHuLVCx";
+  CONFIG.DND5E.weaponIds.heavyw =
+    "Compendium.fvtt-me5e.items-me5e.Item.it7NrU6lg2OrQF0S";
   // Add ME5E misc equipment types
-  foundry.utils.mergeObject(CONFIG.DND5E.miscEquipmentTypes, {
-    armormod: { label: "Armor Mod" },
-    bodyarmor: { label: "Body Armor" },
-    weaponmod: { label: "Weapon Mod" },
-    program: { label: "Program" },
-  });
 
-  // Add ME5E weapon types
-  foundry.utils.mergeObject(CONFIG.DND5E.weaponTypes, {
-    assaultr: { label: "Assault Rifle" },
-    heavyp: { label: "Heavy Pistol" },
-    smg: { label: "Submachine Gun" },
-    shotgun: { label: "Shotgun" },
-    sniperr: { label: "Sniper Rifle" },
-  });
+  CONFIG.DND5E.miscEquipmentTypes.armormod = "Armor Mod";
+  CONFIG.DND5E.miscEquipmentTypes.bodyarmor = "Body Armor";
+  CONFIG.DND5E.miscEquipmentTypes.weaponmod = "Weapon Mod";
+  CONFIG.DND5E.miscEquipmentTypes.program = "Program";
 
-  // Add ME5E weapon types
-  foundry.utils.mergeObject(CONFIG.DND5E.toolTypes, {
-    starship: { label: "Starship" },
-  });
+  // Add ME5E tool categories
+  CONFIG.DND5E.toolTypes.starship = "Starship Systems";
+
+  //Add ME5E tool proficiencies IDs
+  CONFIG.DND5E.toolIds.armorsmithtool =
+    "Compendium.fvtt-me5e.items-me5e.Item.fi5seHBSJlL5oezH";
+  CONFIG.DND5E.toolIds.chemisttool =
+    "Compendium.fvtt-me5e.items-me5e.Item.IRuDN4PHt39s4OMv";
+  CONFIG.DND5E.toolIds.hackingtool =
+    "Compendium.fvtt-me5e.items-me5e.Item.tjzo96t71HDQoBVT";
+  CONFIG.DND5E.toolIds.medicaltool =
+    "Compendium.fvtt-me5e.items-me5e.Item.x69e18GsSnjwmYXw";
+  CONFIG.DND5E.toolIds.starshipdrive =
+    "Compendium.fvtt-me5e.items-me5e.Item.XgQfWGXFkjKw3X8T";
+  CONFIG.DND5E.toolIds.starshipews =
+    "Compendium.fvtt-me5e.items-me5e.Item.1yiADlMcYaixDzPR";
+  CONFIG.DND5E.toolIds.starshiphelm =
+    "Compendium.fvtt-me5e.items-me5e.Item.pMmFHFM2ICJxvk4g";
+  CONFIG.DND5E.toolIds.starshipnavg =
+    "Compendium.fvtt-me5e.items-me5e.Item.z3zCYhwZg8eDFROr";
+  CONFIG.DND5E.toolIds.starshipssc =
+    "Compendium.fvtt-me5e.items-me5e.Item.KIAoHK6rtipLFmUY";
+  CONFIG.DND5E.toolIds.starshipweap =
+    "Compendium.fvtt-me5e.items-me5e.Item.nttGDd2AGMe821lm";
+  CONFIG.DND5E.toolIds.weaponsmithtool =
+    "Compendium.fvtt-me5e.items-me5e.Item.OWvQBq1ylb9FafvH";
 });
-
-/* 
-
-DND5E.toolProficiencies["herb"] = "Chemist's Supplies";
-DND5E.toolProficiencies["navg"] = "Starship System (Navigation)";
-DND5E.toolProficiencies["pois"] = "Brewer's Supplies"
-DND5E.toolProficiencies["aswb"] = "Armorsmith's Workbench";
-DND5E.toolProficiencies["cook"] = "Cook's Utensils";
-DND5E.toolProficiencies["h4ck"] = "Hacking Tools";
-DND5E.toolProficiencies["mdcn"] = "Medical Kit";
-DND5E.toolProficiencies["pntr"] = "Painter's Supplies";
-DND5E.toolProficiencies["ssdr"] = "Starship Systems (Drive)";
-DND5E.toolProficiencies["sshe"] = "Starship Systems (Helm)";
-DND5E.toolProficiencies["sssc"] = "Starship Systems (SSC)";
-DND5E.toolProficiencies["ssew"] = "Starship Systems (EWS)";
-DND5E.toolProficiencies["sswp"] = "Starship Systems (Weapons)";
-DND5E.toolProficiencies["tail"] = "Tailor's Tools";
-DND5E.toolProficiencies["tink"] = "Tinker's Tools";
-DND5E.toolProficiencies["wswb"] = "Weaponsmith's Workbench"; 
-
-
-  // Update weapon type map
-  CONFIG.DND5E.weaponTypeMap.assaultr;
-  ("ranged");
-  CONFIG.DND5E.weaponTypeMap.heavyp;
-  ("ranged");
-  CONFIG.DND5E.weaponTypeMap.smg;
-  ("ranged");
-  CONFIG.DND5E.weaponTypeMap.shotgun;
-  ("ranged");
-  CONFIG.DND5E.weaponTypeMap.sniperr;
-  ("ranged");
-  CONFIG.DND5E.weaponTypeMap.heavyw;
-  ("ranged");
-  // Update weapon prof map
-  CONFIG.DND5E.weaponProficienciesMap.assaultr;
-  ("sim");
-  CONFIG.DND5E.weaponProficienciesMap.heavyp;
-  ("sim");
-  CONFIG.DND5E.weaponProficienciesMap.smg;
-  ("sim");
-  CONFIG.DND5E.weaponProficienciesMap.shotgun;
-  ("sim");
-  CONFIG.DND5E.weaponProficienciesMap.sniperr;
-  ("sim");
-  CONFIG.DND5E.weaponProficienciesMap.heavyw;
-  ("sim");
-  };*/
