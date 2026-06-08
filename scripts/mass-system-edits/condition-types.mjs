a5389f6df0aaaa6f6cdf3afb4edbc68930a8d56d
Parker <23155686+sparkcity@users.noreply.github.com>
2026-06-08 10:47:26 -0400

    BUG57-updating condition types mjs. Final validation.

--- scripts/mass-system-edits/condition-types.mjs
+++ scripts/mass-system-edits/condition-types.mjs
@@ -5,52 +5,52 @@ export const loadConditionTypesEdits = function () {
     indoctrinated: {
       name: "ME5E.conditionTypes.condTypeIndoc",
       img: "modules/fvtt-me5e/assets/icons/status-indoctrinated.webp",
-      reference: "For each 24 hour period a creature spends within the presence of an indoctrinating source, the creature must make a DC 10 Wisdom saving throw. On a failed save, the creature suffers one level of indoctrination. If an already indoctrinated creature spends another 24 hour period within the presence of Reaper technology, the DC of the saving throw increases as described in the indoctrination table."
+      reference: "Compendium.fvtt-me5e.rules.JournalEntry.ylamfcORxRL1IDwJ.JournalEntryPage.IiZwuTq3Cz9ESej5"
     },
     lifted: {
       name: "ME5E.conditionTypes.condTypeLift",
       img: "modules/fvtt-me5e/assets/icons/status-lifted.webp",
-      reference: "Compendium.fvtt-me5e.rules.JournalEntry.ylamfcORxRL1IDwJ"
+      reference: "Compendium.fvtt-me5e.rules.JournalEntry.ylamfcORxRL1IDwJ.JournalEntryPage.Iw0QNMOzHeWu8o0r"
     },
     primed: {
       name: "ME5E.conditionTypes.condTypePrime",
       img: "modules/fvtt-me5e/assets/icons/status-primed.webp",
-      reference: ""
+      reference: "Compendium.fvtt-me5e.rules.JournalEntry.ylamfcORxRL1IDwJ.JournalEntryPage.2EcLvQyjvXTRUTc1"
     },
     primedCold: {
       name: "ME5E.conditionTypes.condTypePrimeCold",
       img: "modules/fvtt-me5e/assets/icons/status-primed.webp",
-      reference: ""
+      reference: "Compendium.fvtt-me5e.rules.JournalEntry.ylamfcORxRL1IDwJ.JournalEntryPage.6L4df8TAWjyVor2S"
     },
     primedFire: {
       name: "ME5E.conditionTypes.condTypePrimeFire",
       img: "modules/fvtt-me5e/assets/icons/status-primed.webp",
-      reference: ""
+      reference: "Compendium.fvtt-me5e.rules.JournalEntry.ylamfcORxRL1IDwJ.JournalEntryPage.SSCgKjAQYEmHpM4x"
     },
     primedForce: {
       name: "ME5E.conditionTypes.condTypePrimeForce",
       img: "modules/fvtt-me5e/assets/icons/status-primed.webp",
-      reference: ""
+      reference: "Compendium.fvtt-me5e.rules.JournalEntry.ylamfcORxRL1IDwJ.JournalEntryPage.jwpKtKoaESvNzL64"
     },
     primedLightning: {
       name: "ME5E.conditionTypes.condTypePrimeLightning",
       img: "modules/fvtt-me5e/assets/icons/status-primed.webp",
-      reference: ""
+      reference: "Compendium.fvtt-me5e.rules.JournalEntry.ylamfcORxRL1IDwJ.JournalEntryPage.ccHlZkRek7hdmttl"
     },
     primedNecrotic: {
       name: "ME5E.conditionTypes.condTypePrimeNecrotic",
       img: "modules/fvtt-me5e/assets/icons/status-primed.webp",
-      reference: ""
+      reference: "Compendium.fvtt-me5e.rules.JournalEntry.ylamfcORxRL1IDwJ.JournalEntryPage.rKhDN6YV1HsDCh6B"
     },
     primedRadiant: {
       name: "ME5E.conditionTypes.condTypePrimeRadiant",
       img: "modules/fvtt-me5e/assets/icons/status-primed.webp",
-      reference: ""
+      reference: "Compendium.fvtt-me5e.rules.JournalEntry.ylamfcORxRL1IDwJ.JournalEntryPage.qmK0oFGVRNttOKA7"
     },
     targeting: {
       name: "ME5E.conditionTypes.condTypeTarget",
       img: "modules/fvtt-me5e/assets/icons/status-targeting.webp",
-      reference: ""
+      reference: "Compendium.fvtt-me5e.rules.JournalEntry.ylamfcORxRL1IDwJ.JournalEntryPage.ZY9ii6Fw13HYK8jC"
     },
   };
 
